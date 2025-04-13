719eb1ad344730f11f77a1102d9468e149708384
Nasim <mdnasimhosen333@gmail.com>
2025-04-13 17:39:05 +0600

    solved problem 5 using another way

--- capital_small_digit.c
+++ capital_small_digit.c
@@ -1,16 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     char ch;
     scanf("%c", &ch);
-    if(ch >= 'a' && ch <= 'z'){
-        printf("ALPHA\n");
-        printf("IS SMALL\n");
-    }else if(ch >= 'A' && ch <= 'Z'){
-        printf("ALPHA\n");
-        printf("IS CAPITAL\n");
-    }else if(ch >= '0' && ch <= '9'){
+    // if(ch >= 'a' && ch <= 'z'){
+    //     printf("ALPHA\n");
+    //     printf("IS SMALL\n");
+    // }else if(ch >= 'A' && ch <= 'Z'){
+    //     printf("ALPHA\n");
+    //     printf("IS CAPITAL\n");
+    // }else if(ch >= '0' && ch <= '9'){
+    //     printf("IS DIGIT\n");
+    // }
+
+    // Another one
+    if (ch >= '0' && ch <= '9')
+    {
         printf("IS DIGIT\n");
+    }
+    else
+    {
+        printf("ALPHA\n");
+        if (ch >= 'a' && ch <= 'z')
+        {
+            printf("IS SMALL\n");
+        }
+        else
+        {
+            printf("IS CAPITAL\n");
+        }
     }
     return 0;
 }
