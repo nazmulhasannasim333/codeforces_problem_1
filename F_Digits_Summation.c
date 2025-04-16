755af3d663c696ee227f3b2715c4637f526c2513
Nasim <mdnasimhosen333@gmail.com>
2025-04-16 15:48:00 +0600

    digit summation solved

--- F_Digits_Summation.c
+++ F_Digits_Summation.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-
+    long long int n, m;
+    scanf("%lld %lld", &n, &m);
+    long long int sum = 0;
+    long long a = n % 10;
+    long long b = m % 10;
+    sum = a + b;
+    printf("%lld\n", sum);
     return 0;
 }
