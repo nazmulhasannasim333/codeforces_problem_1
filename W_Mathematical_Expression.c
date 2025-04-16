2a750f258b9362fa53867c5db9337efb53fbdff9
Nasim <mdnasimhosen333@gmail.com>
2025-04-16 23:29:00 +0600

    math expression

---
+++ W_Mathematical_Expression.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    char S, Q;
+    scanf("%d %c %d %c %d", &A, &S, &B, &Q, &C);
+    long long int result = 0;
+    if (S == '+')
+    {
+        result = A + B;
+    }
+    else if (S == '-')
+    {
+        result = A - B;
+    }
+    else if (S == '*')
+    {
+        result = A * B;
+    }
+
+    if (result == C)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("%lld\n", result);
+    }
+
+    return 0;
+}
