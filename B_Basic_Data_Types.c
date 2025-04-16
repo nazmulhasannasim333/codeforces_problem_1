f3ecf51ec2bf848311974dc0c3f958700db4b4f2
Nasim <mdnasimhosen333@gmail.com>
2025-04-16 15:35:52 +0600

    first problem solved

---
+++ B_Basic_Data_Types.c
@@ -0,0 +1,29 @@
+// #include<stdio.h>
+
+// int main(){
+//     int a;
+//     long long int b;
+//     char c;
+//     float d;
+//     double e;
+//     scanf("%d %lld %c %f %lf", &a, &b, &c, &d, &e);
+//     printf("%d\n%lld\n%c\n%.2f\n%.1lf", a, b, c, d, e);
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+
+    scanf("%d %lld %c %f %lf", &a, &b, &c, &d, &e);
+
+    printf("%d\n%lld\n%c\n%.2f\n%.1lf\n", a, b, c, d, e);
+
+    return 0;
+}
