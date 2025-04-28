8237a2ccec8e0e1867ecfcceada7d879351b0bb8
Nasim <mdnasimhosen333@gmail.com>
2025-04-28 19:12:30 +0600

    optimize summation calculation using formula and add problem metadata

--- G_Summation_from_1_to_N.c
+++ G_Summation_from_1_to_N.c
@@ -2,29 +2,12 @@
 
 int main()
 {
-    long long int n;
-    scanf("%lld", &n);
-    long long int sum = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        sum += i;
-    }
+    long long N;
+    scanf("%lld", &N);
+
+    long long sum = (N * (N + 1)) / 2;
+
     printf("%lld\n", sum);
+
     return 0;
 }
-
-// #include <stdio.h>
-
-// int main()
-// {
-//     long long int n; // Use long long int to handle large numbers
-//     scanf("%lld", &n);
-
-//     // Calculate the sum using the formula
-//     long long int sum = (n * (n + 1)) / 2;
-
-//     // Print the result
-//     printf("%lld\n", sum);
-
-//     return 0;
-// }
