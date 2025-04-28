380045e6d00215dec82e0705006f97700b909af3
Nasim <mdnasimhosen333@gmail.com>
2025-04-28 19:07:28 +0600

    refactor variable names for clarity and improve output formatting

--- B_Basic_Data_Types.c
+++ B_Basic_Data_Types.c
@@ -11,19 +11,45 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a;
+//     long long int b;
+//     char c;
+//     float d;
+//     double e;
+
+//     scanf("%d %lld %c %f %lf", &a, &b, &c, &d, &e);
+
+//     printf("%d\n%lld\n%c\n%.2f\n%.1lf\n", a, b, c, d, e);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    int a;
-    long long int b;
-    char c;
-    float d;
-    double e;
+    int intValue;
+    long long longLongValue;
+    char charValue;
+    float floatValue;
+    double doubleValue;
 
-    scanf("%d %lld %c %f %lf", &a, &b, &c, &d, &e);
+    scanf("%d %lld %c %f %lf", &intValue, &longLongValue, &charValue, &floatValue, &doubleValue);
 
-    printf("%d\n%lld\n%c\n%.2f\n%.1lf\n", a, b, c, d, e);
+    printf("%d\n", intValue);
+    printf("%lld\n", longLongValue);
+    printf("%c\n", charValue);
+    printf("%.2f\n", floatValue);
+
+    if (doubleValue == 0.0)
+    {
+        doubleValue = 0.0;
+    }
+    printf("%.1lf\n", doubleValue);
 
     return 0;
 }
